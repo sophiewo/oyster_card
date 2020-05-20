8f85d5426580a3d360f0915379018049e1f7ff64
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-05-20 18:19:21 +0100

    Created card with initial balance of 0

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# oyster_card
+# Oyster Card App
+
+## User Stories
+
+```
+In order to use public transport
+As a customer
+I want money on my card
+```
+
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+
+  subject = Oystercard.new
+
+  it 'should initialize with a balance of 0' do
+    # balance = 0
+    # oystercard = Oystercard.new
+    expect(Oystercard.new.balance).to eq(0)
+  end
 end
