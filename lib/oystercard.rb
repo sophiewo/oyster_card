96c0536f47c476fe00d8f309d626cf6136709f53
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-05-21 12:03:07 +0100

    Add maximum top up amount with guard condition

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,7 @@
 class Oystercard
 
+  MAXIMUM_BALANCE = 90
+
   attr_reader :balance
 
   def initialize
@@ -7,6 +9,8 @@ class Oystercard
   end
 
   def top_up(amount)
+    fail "Top up amount exceeds maximum of #{MAXIMUM_BALANCE}" if balance + amount >  MAXIMUM_BALANCE
     @balance += amount
   end
+  
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,6 @@ require 'oystercard'
 
 describe Oystercard do
 
-  subject = Oystercard.new
-
   it 'should initialize with a balance of 0' do
     expect(subject.balance).to eq(0)
   end
@@ -11,10 +9,14 @@ describe Oystercard do
   describe '#top_up' do 
     it 'should add £10 to oystercard balance' do
       amount = 10
-      expect { subject.top_up(amount) }.to change { subject.balance }.by(amount)
+      expect { subject.top_up(amount) }.to change { subject.balance }.by amount
     end
 
-
+    it 'should raise error if balance exceeds maximum limit' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect { subject.top_up(1)}.to raise_error "Top up amount exceeds maximum of #{maximum_balance}"
+    end
   end
 
 end
